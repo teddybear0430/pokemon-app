684e43c0a818759ee906a5e435b91d7d6a55e4bb
teddybear0430 <teddybear0430@hotmail.com>
2020-02-04 23:22:54 +0900

    自己紹介とアイコンのurlのカラムを追加するマイグレーションファイルを作成。

---
+++ src/database/migrations/2020_02_02_153847_change_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('icon_url')->nullable();
+            $table->string('introduction', 150)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('icon_url');
+            $table->dropColumn('introduction');
+        });
+    }
+}
