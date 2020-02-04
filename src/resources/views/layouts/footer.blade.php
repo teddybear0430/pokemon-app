005aee31b181f0ecfc1828ae47e8269d3428751e
teddybear0430 <teddybear0430@hotmail.com>
2020-02-05 01:34:05 +0900

    フッターを作成。

--- src/resources/views/layouts/footer.blade.php
+++ src/resources/views/layouts/footer.blade.php
@@ -1,4 +1,7 @@
 @section ('footer')
     <footer id="footer">
+        <span class="copyright">
+            <?php echo "Copyright ©". date('Y') ." ポケモン【剣盾】育成論投稿サイト All Rights Reserved."; ?>
+        </span>
     </footer>
 @endsection
