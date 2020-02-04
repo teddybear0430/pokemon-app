9091b89e7b93f1d310a43f485a0673cd574243c3
teddybear0430 <teddybear0430@hotmail.com>
2020-02-04 23:24:08 +0900

    ルーティングを追加。

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,6 +16,8 @@ Route::get('/', 'HomeController@index')->name('home');
 
 // ユーザーページ
 Route::get('/user/{id}', 'UserController@show')->name('user.show');
+Route::get('/user/{id}/edit', 'UserController@edit')->name('user.edit');
+Route::patch('/user/{id}', 'UserController@update')->name('user.update');
 
 // 育成論のページ
 Route::get('/theory/create', 'TheoryController@create')->name('theory.create');
@@ -23,7 +25,7 @@ Route::post('/theory', 'TheoryController@store')->name('theory.store');
 Route::get('/theory/{id}', 'TheoryController@show')->name('theory.show');
 Route::get('/theory/{id}/edit', 'TheoryController@edit')->name('theory.edit');
 Route::patch('/theory/{id}', 'TheoryController@update')->name('theory.update');
-Route::delete('/theory/{theory}', 'TheoryController@destroy')->name('theory.destroy');
+Route::delete('/theory/{id}', 'TheoryController@destroy')->name('theory.destroy');
 
 // 認証関連
 Auth::routes();
