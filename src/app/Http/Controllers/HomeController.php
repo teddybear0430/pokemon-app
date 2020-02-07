18d847ef35e05fcb78fb9584794a829bd594ef37
teddybear0430 <teddybear0430@hotmail.com>
2020-02-08 00:42:17 +0900

    ポケモンの名前で育成論を検索できる機能を実装。

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -4,20 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Theory;
+use App\Pokemon;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+    private $types;
+    private $personalities;
+
+    public function __construct() 
+    {
+        $this->types = config('types');
+        $this->personalities = config('personalities');
+    }
+
     public function index(Request $request)
     {
-        $theories = Theory::orderBy('created_at', 'desc')->get();
+        $query = Theory::query();
+        $keyword = $request->keyword;
+
+        if (!empty($keyword)) {
+            $query->whereHas('pokemon', function($query) use($keyword) {
+                $query->where('pokemon_name', 'like', '%'.$keyword.'%');
+            });
+        }
+
+        $theories = $query->orderBy('created_at', 'desc')->paginate(5);
 
         return view('home', [
-            'theories' => $theories
+            'theories' => $theories,
         ]);
     }
+
 }
