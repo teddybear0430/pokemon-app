b22c962f79b5b7afc2368a20e37945fc2f27f76d
teddybear0430 <teddybear0430@hotmail.com>
2020-02-10 21:53:13 +0900

    deferがついているとjQueryがうまく動かなかったので消した。

--- src/resources/views/layouts/head.blade.php
+++ src/resources/views/layouts/head.blade.php
@@ -9,7 +9,7 @@
     <title>ポケモン【剣盾】育成論投稿サイト</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
