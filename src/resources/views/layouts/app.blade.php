9899d51cfeb6e20b87e3c53a1626fe3e1a2b7d39
teddybear0430 <teddybear0430@hotmail.com>
2020-02-10 21:58:37 +0900

    <div>を削除。

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -2,12 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @yield ('head')
 <body>
-    <div id="app">
-            @yield ('header')
-        <main class="py-4">
-            @yield ('content')
-        </main>
-            @yield ('footer')
-    </div>
+    @yield ('header')
+    <main class="py-4">
+        @yield ('content')
+    </main>
+    @yield ('footer')
 </body>
 </html>
